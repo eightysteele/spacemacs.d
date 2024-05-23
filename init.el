35c5c1e8ef12afdffc616c60c0ce20e9ff666bb3
Aaron Steele <eightysteele@gmail.com>
2024-05-23 09:33:36 -0700

    use SPACEMACSDIR

--- init.el
+++ init.el
@@ -29,7 +29,8 @@ This function should only modify configuration layer settings."
 
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. "~/.mycontribs/")
-   dotspacemacs-configuration-layer-path '("~/.config/spacemacs.d/layers/")
+   dotspacemacs-configuration-layer-path '(concat (getenv "SPACEMACSDIR") "/layers/")
+   
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
